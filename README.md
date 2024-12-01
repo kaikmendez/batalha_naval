ebae135122033d1e7a64cc9be52c3ccaeb92f518
Kayque <kayquemendes@live.com>
2024-12-01 17:54:15 -0300

    Criação de funções de criação e exibição do tabuleiro.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# batalha_naval
+# Batalha Naval
 Repositório criado para armazenar projeto de criação do jogo batalha naval utilizando python.
--- jogo.py
+++ jogo.py
@@ -1,9 +1,15 @@
 import ramdom as rm
 
-board = [
-    ["~"] * 20
-    for _ in range(20)       
-]
+def criar_tabuleiro():
+    return [
+        ["~"] * 20
+         for _ in range(20)
+    ]
 
-for row in (board):
-       print(row)
+tabuleiro = criar_tabuleiro()
+
+def exibir_tabuleiro(tabuleiro):
+    for row in tabuleiro:
+        print("  ".join(row))
+
+exibir_tabuleiro(tabuleiro)
